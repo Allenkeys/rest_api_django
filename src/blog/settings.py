3e022b9c71dacc059c3de0790bbe748007cd3d7a
Joshua <ezejoshuac@gmail.com>
2022-06-30 17:37:11 +0100

    Added rest framework to installed apps field

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #apps created by me
     'api_app',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
